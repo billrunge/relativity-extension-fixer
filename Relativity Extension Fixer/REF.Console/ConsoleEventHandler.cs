b83094730d7fb8fc43665ba6ab0cda43e9f37def
billrunge <billrunge@gmail.com>
2018-07-26 21:47:37 -0500

    The event handler now puts the jobs in the queue. However, I cheated and hardcoded the data source search artifactID. Next step: retrieve search artifactId from field collection and use that in insert query

--- Relativity Extension Fixer/REF.Console/ConsoleEventHandler.cs
+++ Relativity Extension Fixer/REF.Console/ConsoleEventHandler.cs
@@ -14,17 +14,86 @@ namespace REF.Console
     [System.Runtime.InteropServices.Guid("67bf919b-b566-4204-813c-ac4785ed17ef")]
     public class ConsoleEventHandler : kCura.EventHandler.ConsoleEventHandler
     {
+        private const String CONSOLE_TITLE = "Extension Fixer";
+        private const String INSERT_JOB_BUTTON_NAME = "_insertJobButton";
+        private const String INSERT_JOB_DISPLAY_TEXT = "Start Job";
+        private const String INSERT_JOB_TOOL_TIP = "Begin Extension Fixer Job";
+        private const String APPLICATION_GUID = "250a9e9a-ed2e-43b7-8467-06d7b41a77c1";
+        public static readonly Guid SEARCH_FIELD_GUID = new Guid("A36C14BE-621C-442C-8A0B-4450DB1391E9");
+
+        private const String JOB_EXISTS_QUERY = "SELECT COUNT(*) FROM [ExtensionFixerQueue] WHERE [WorkspaceArtifactID] = @WorkspaceArtifactID AND [JobArtifactID] = @JobArtifactID";
+        //You want to ensure that there is only one job per patient at any given time.
+        private const String INSERT_JOB_QUERY = @"
+                IF NOT EXISTS(SELECT TOP 1 * FROM [ExtensionFixerQueue] WHERE [WorkspaceArtifactID] = @WorkspaceArtifactID AND [JobArtifactID] = @JobArtifactID)
+                BEGIN
+                INSERT INTO [ExtensionFixerQueue] (WorkspaceArtifactID, JobArtifactID, SourceSearchArtifactID, [Status], CreatedOn, LastModifiedOn)
+                Values (@WorkspaceArtifactID, @JobArtifactID, 1042643, 0, GETUTCDATE(),GETUTCDATE())
+                END";
+
         public override kCura.EventHandler.Console GetConsole(kCura.EventHandler.ConsoleEventHandler.PageEvent pageEvent)
         {
+            //Construct a console object to build the console appearing in the UI.
             kCura.EventHandler.Console returnConsole = new kCura.EventHandler.Console();
+            returnConsole.Items = new List<kCura.EventHandler.IConsoleItem>();
+            returnConsole.Title = CONSOLE_TITLE;
+
+            //Construct the submit job button.
+            kCura.EventHandler.ConsoleButton submitJobButton = new kCura.EventHandler.ConsoleButton();
+            submitJobButton.Name = INSERT_JOB_BUTTON_NAME;
+            submitJobButton.DisplayText = INSERT_JOB_DISPLAY_TEXT;
+            submitJobButton.ToolTip = INSERT_JOB_TOOL_TIP;
+            submitJobButton.RaisesPostBack = true;
+
+            //If a job is already in the queue, change the text and disable the button.
+            if (pageEvent == PageEvent.PreRender)
+            {
+                System.Data.SqlClient.SqlParameter workspaceArtifactIDParam = new System.Data.SqlClient.SqlParameter("@WorkspaceArtifactID", System.Data.SqlDbType.Int);
+                workspaceArtifactIDParam.Value = this.Helper.GetActiveCaseID();
+
+                System.Data.SqlClient.SqlParameter jobArtifactIDParam = new System.Data.SqlClient.SqlParameter("@JobArtifactID", System.Data.SqlDbType.Int);
+                jobArtifactIDParam.Value = this.ActiveArtifact.ArtifactID;
+
+                int jobCount = this.Helper.GetDBContext(-1).ExecuteSqlStatementAsScalar<Int32>(JOB_EXISTS_QUERY, new System.Data.SqlClient.SqlParameter[] { workspaceArtifactIDParam, jobArtifactIDParam });
+
+                //Use the helper function to check if a job currently exists. Set Enabled to the opposite value.
+                if (jobCount > 0)
+                {
+                    submitJobButton.Enabled = false;
+                }
+                else
+                {
+                    submitJobButton.Enabled = true;
+                }
+
+            }
+
+            //Add the buttons to the console.
+            returnConsole.Items.Add(submitJobButton);
+
             return returnConsole;
+
+
         }
 
         public override void OnButtonClick(kCura.EventHandler.ConsoleButton consoleButton)
         {
             switch (consoleButton.Name)
             {
-                //Handle each Button's functionality
+                case INSERT_JOB_BUTTON_NAME:
+                    //The user clicked the button for the insert job so add the job to the queue table on the EDDS database.
+                    System.Data.SqlClient.SqlParameter workspaceArtifactIDParam = new System.Data.SqlClient.SqlParameter("@WorkspaceArtifactID", System.Data.SqlDbType.Int);
+                    workspaceArtifactIDParam.Value = this.Helper.GetActiveCaseID();
+                    System.Data.SqlClient.SqlParameter jobArtifactIDParam = new System.Data.SqlClient.SqlParameter("@JobArtifactID", System.Data.SqlDbType.Int);
+                    jobArtifactIDParam.Value = this.ActiveArtifact.ArtifactID;
+
+
+                    //string searchArtifactID = this.ActiveArtifact.Fields.;
+
+                    //searchArtifactID = this.ActiveArtifact.
+
+                    this.Helper.GetDBContext(-1).ExecuteNonQuerySQLStatement(INSERT_JOB_QUERY, new System.Data.SqlClient.SqlParameter[] { workspaceArtifactIDParam, jobArtifactIDParam });
+
+                    break;
             }
         }
 
@@ -38,20 +107,10 @@ namespace REF.Console
             get
             {
                 kCura.EventHandler.FieldCollection retVal = new kCura.EventHandler.FieldCollection();
+                retVal.Add(new kCura.EventHandler.Field(SEARCH_FIELD_GUID));
                 return retVal;
             }
         }
 
-        public static void RunSavedSearch(IRSAPIClient proxy, int artifactId)
-        {
-            // proxy.APIOptions.WorkspaceID is already set
-            Query<Document> query = new Query<Document>
-            {
-                Condition = new SavedSearchCondition(artifactId),
-                Fields = kCura.Relativity.Client.DTOs.FieldValue.SelectedFields
-            };
-
-            ResultSet<Document> docResults = proxy.Repositories.Document.Query(query);
-        }
     }
 }
